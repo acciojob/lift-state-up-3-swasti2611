cd1749d56f9e5711b227b78e084531e078347619
Swati jagtap <swatijagtap5060@gmail.com>
2024-03-02 15:28:08 +0530

    submitted-

--- src/components/ChildComponent1.js
+++ src/components/ChildComponent1.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const ChildComponent1 = (props) => {
-    function handleClick(){
-      console.log(props.value)
-  props.handleOption(props.value)
+    function handleClick(value){
+      console.log(value)
+  props.handleOption(value)
     }
   return (
     <div >
       <div style={{width:'900px',height:'200px',textAlign:'center',backgroundColor:'orange',color:'black',marginLeft:'50px',marginTop:'50px'}}>
                <h1 >Child Component 1</h1> 
-               <button onClick={handleClick}>Option 1</button> 
+               <button onClick={(e)=>handleClick(e.target.innerHTML)}>Option 1</button> 
       </div>
     </div>
   )
--- src/components/ChildComponent2.js
+++ src/components/ChildComponent2.js
@@ -3,13 +3,13 @@ import React from 'react'
 const ChildComponent2 = (props) => {
     function handleClick(value){
         console.log(value)
-        props.handleOption(props.value)
+        props.handleOption(value)
           }
         return (
           <div >
             <div style={{width:'900px',height:'200px',backgroundColor:'yellow',textAlign:'center',color:'black', marginLeft:'50px',marginTop:'50px'}}>
                      <h1>Child Component 1</h1> 
-                     <button onClick={handleClick} >Option 2</button>
+                     <button onClick={(e)=>handleClick(e.target.innerHTML)} >Option 2</button>
             </div>
           </div>
         )
